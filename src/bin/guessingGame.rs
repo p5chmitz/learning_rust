7ec12661b26d5be0a7f49837d0003748a8146af5
p5chmitz <petermschmitz@gmail.com>
2023-10-17 20:15:53 -0700

    Adds guessingGame.rs

---
+++ src/bin/guessingGame.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    println!("Enter number: ");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failure. You're a failure.");
+    println!("You guessed: {guess}");
+
+}
