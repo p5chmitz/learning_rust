2a03d6bd463e97ac0ebff7c1339c7edc80c43248
p5chmitz <petermschmitz@gmail.com>
2023-10-21 16:30:37 -0700

    Finishes the guessing game!

--- src/bin/guessing_game.rs
+++ src/bin/guessing_game.rs
@@ -1,19 +1,31 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Enter number: ");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failure. You're a failure.");
-    println!("You guessed: {guess}");
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too smol"),
-        Ordering::Greater => println!("Too larg"),
-        Ordering::Equal => println!("You guessed it!")
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Enter guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failure. You're a failure.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Thats not a number, try again.");
+                continue;
+            }
+        };
+        match guess.cmp(&secret_number) {
+            //guess and secret_number must be of the same type
+            Ordering::Less => println!("Too smol"),
+            Ordering::Greater => println!("Too larg"),
+            Ordering::Equal => {
+                println!("You guessed it!");
+                break;
+            }
+        }
     }
 }
