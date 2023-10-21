a65751450242068fbc8035ce68fd8f0bc3079387
p5chmitz <petermschmitz@gmail.com>
2023-10-20 22:23:01 -0700

    End of the night save

--- src/bin/guessing_game.rs
+++ src/bin/guessing_game.rs
@@ -1,5 +1,6 @@
-use rand::Rng;
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -10,5 +11,9 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failure. You're a failure.");
     println!("You guessed: {guess}");
-
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too smol"),
+        Ordering::Greater => println!("Too larg"),
+        Ordering::Equal => println!("You guessed it!")
+    }
 }
