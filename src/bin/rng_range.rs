bca18a925e4eb2700f9838554fad24d79f7e3ad6
p5chmitz <petermschmitz@gmail.com>
2023-10-20 21:51:57 -0700

    Adds random number generator

---
+++ src/bin/rng_range.rs
@@ -0,0 +1,22 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+    let a: i32 = rand::thread_rng().gen_range(1..=10);
+    println!("RANDOM NUMBER: {}", a);
+
+    let mut a: String = String::new();
+    let mut b: String = String::new();
+    println!("Whats the range we're dealing with here?\n\
+        Enter the the low and high limits separately as integers followed by an \"enter\".");
+    io::stdin()
+        .read_line(&mut a)
+        .expect("Why must you constantly disappoint me?");
+    io::stdin()
+        .read_line(&mut b)
+        .expect("You've always been a failure");
+    let x: i32 = a.trim().parse().expect("Not an integer you dingus");
+    let y: i32 = b.trim().parse().expect("Not an integer you dope");
+    let z: i32 = rand::thread_rng().gen_range(x..=y);
+    println!("The mystical prophecy foretells: {}", z);
+}
