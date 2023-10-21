78da01da52e4e78f9ac2cbf4506b685dcd2f45ff
p5chmitz <petermschmitz@gmail.com>
2023-10-20 19:54:25 -0700

    Updates guessing game

--- src/bin/guessing_game.rs
+++ src/bin/guessing_game.rs
@@ -5,8 +5,7 @@ fn main() {
     println!("Enter number: ");
     let mut guess = String::new();
     io::stdin()
-        .read_line(& mut guess)
+        .read_line(&mut guess)
         .expect("Failure. You're a failure.");
     println!("You guessed: {guess}");
-
 }
