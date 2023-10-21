34d8ea5d9de671b1b27f33b5fe2fa8418ac2d19d
p5chmitz <petermschmitz@gmail.com>
2023-10-20 20:34:35 -0700

    Updates Cargo.toml

--- src/bin/guessing_game.rs
+++ src/bin/guessing_game.rs
@@ -1,8 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
     println!("Enter number: ");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
