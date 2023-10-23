21bf0c79817ca59b221aed60dd496f4042881e78
p5chmitz <petermschmitz@gmail.com>
2023-10-22 21:00:08 -0700

    Adds comments and nested object parsing
    This commit adds work with the JSON crate in the Variables program
    that creates two JSON objects and parses them with examples on how
    to access nested objects and values.

--- src/bin/variables.rs
+++ src/bin/variables.rs
@@ -1,12 +1,14 @@
 use json;
-use json::JsonValue;
+use json::{JsonValue, object};
 
 fn main() {
+    /** Basic declaration, instantiation, and printing */
     let mut x:i32 = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The NEW value of x is: {}", x);
 
+    /** Declaring and using constants */
     const PIE: f32 = 3.1459;
     const TWO_PIE: f32 = PIE * 2.0;
     let mon: i32 = 12;
@@ -14,6 +16,8 @@ fn main() {
     println!("\nCombining {} with {} results in {}.", mon, yr, (mon + yr));
     println!("The value of pie is roughly {}, and double that is roughly {}.", PIE, TWO_PIE);
 
+    /** Example of (over)shadowing where multiple variables are created
+     with the same name, but hold different values .*/
     let n: u32 = 3;
     println!("\nWhat is n?! Easy, its {}.", n);
     {
@@ -24,30 +28,50 @@ fn main() {
     let n: f32 = 32.3;
     println!("What is n?! Easy, its {}.", n);
 
-    //let mut spaces: String = String::new();
-    let spaces = "   ";
-    let spaces = spaces.len();
+    /** Example of shadowing that allows us to change type */
+    let spaces: &str = "   ";
+    let spaces: usize = spaces.len();
     println!("\nThe value of spaces is \"{}\".", spaces);
 
+    /** This example accomplishes the same thing, but does not use shadowing */
+    let mut phrase: String = String::new();
+    phrase = "Whatever we want it to, now".parse().expect("lol");
+    println!("\nThe value of spaces is \"{}\".", phrase.len());
+
+    /** Example of a whole loop. */
     let mut age: i32 = 0;
     let mut year: i32 = 1983;
     println!("I was born in {}.", year);
     year += 1;
     while year <= 2023 {
         println!("For most of {} I was {}, but at the very end I turned {}.", year, age, (age + 1));
-//        println!("I turned {} at the very end of {}.", age, year);
         age += 1;
         year += 1;
     }
 
+    /** Using the JSON crate */
     let parsed: JsonValue = json::parse(
         r#"
             {
                 "key": "12/23/1983",
-                "anotherKey": "value"
+                "anotherKey": "value",
+                "object": {
+                    "nestedOne": "one",
+                    "nestedTwo": "two"
+                }
             }
         "#
         ).unwrap();
+    let instantiated = object!{
+        "key": "12/23/1983",
+        "anotherKey": "value",
+        "object": {
+            "nestedOne": "one",
+            "nestedTwo": "two"
+        }
+    };
     println!("The \"key's\" value is: \"{}\"", parsed["key"]);
+    println!("For the parsed object, the nested key \"nestedOne\" value is: \"{}\"", (parsed["object"]["nestedOne"]));
+    println!("For the instantiated object, the nested key \"nestedTwo\" value is: \"{}\"", (instantiated["object"]["nestedTwo"]));
 
 }
