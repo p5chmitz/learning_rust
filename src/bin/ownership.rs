47799f71326a76fe3d1b11d92ad6cc98a96fb6f6
p5chmitz <petermschmitz@gmail.com>
2023-10-29 21:22:15 -0700

    Adds ownership junk

--- src/bin/ownership.rs
+++ src/bin/ownership.rs
@@ -1,21 +1,27 @@
 use std::io;
+use std::str;
 
 //Instantiates a String class and binds user input to the variable
-fn new_string_class() {
+fn new_string_class() -> String {
     let mut a: String = String::new();
     io::stdin()
         .read_line(&mut a)
         .expect("Failure. You're a failure.");
+    return a;
 }
 //Instantiates a String class and binds hardcoded literal to the variable
-fn new_string_class_literal() {
-    let mut b: String = String::from("hello");
+fn new_string_class_literal() -> String {
+    let mut a: String = String::from("hello");
+    return a;
 }
 //Creates a new reference and binds hardcoded literal to the variable
 fn new_string_literal() {
-    let a: &str = "hello!";
+    let a: &str = "henlo!";
+    println!("{a}");
 }
 
 fn main() {
-
+    println!("{:?}", new_string_class());
+    println!("{}", new_string_class_literal());
+    new_string_literal();
 }
