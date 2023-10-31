c16ea067576b7df6f157afd67cdb231f5df43b95
p5chmitz <petermschmitz@gmail.com>
2023-10-31 10:30:16 -0700

    Updates guessing game to format prompt and output

--- src/bin/guessing_game.rs
+++ src/bin/guessing_game.rs
@@ -1,12 +1,13 @@
 use rand::Rng;
 use std::cmp::Ordering;
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
     println!("Guess the number!");
     let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
     loop {
-        println!("Enter guess: ");
+        print!("Enter guess: ");
+        io::stdout().flush().unwrap();
         let mut guess: String = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -18,10 +19,10 @@ fn main() {
                 continue;
             }
         };
+        //guess and secret_number must be of the same type
         match guess.cmp(&secret_number) {
-            //guess and secret_number must be of the same type
-            Ordering::Less => println!("Too smol"),
-            Ordering::Greater => println!("Too larg"),
+            Ordering::Less => println!("{} is too smol", x),
+            Ordering::Greater => println!("{} is too larg", x),
             Ordering::Equal => {
                 println!("You guessed it!");
                 break;
