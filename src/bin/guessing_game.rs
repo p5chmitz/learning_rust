8cc95d58ea62a14f2353c6c008a9a5fdbf8edfe1
p5chmitz <petermschmitz@gmail.com>
2023-10-31 11:46:40 -0700

    Adds bounds checking and error messages to guessing game

--- src/bin/guessing_game.rs
+++ src/bin/guessing_game.rs
@@ -4,7 +4,7 @@ use std::io::{self, Write};
 
 fn main() {
     println!("Guess the number!");
-    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
     loop {
         print!("Enter guess: ");
         io::stdout().flush().unwrap();
@@ -12,17 +12,21 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failure. You're a failure.");
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("Thats not a number, try again.");
+                println!("Guess must be an integer between 1-100");
                 continue;
             }
         };
+        if guess <= 0 || guess >= 100 {
+            println!("Out of bounds, must be between 1-100");
+            continue;
+        }
         //guess and secret_number must be of the same type
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("{} is too smol", x),
-            Ordering::Greater => println!("{} is too larg", x),
+            Ordering::Less => println!("{} is too smol", guess),
+            Ordering::Greater => println!("{} is too larg", guess),
             Ordering::Equal => {
                 println!("You guessed it!");
                 break;
