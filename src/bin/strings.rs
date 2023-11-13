af2ad5dd79411d016a14688f0dca7d8b16cff97f
p5chmitz <petermschmitz@gmail.com>
2023-11-12 18:34:02 -0800

    Format changes to strings.rsw

--- src/bin/strings.rs
+++ src/bin/strings.rs
@@ -1,10 +1,11 @@
-
 //The Slice type
 fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
     //For loop iterates over the converted bytes looking for the first "space"
-    for (i, &item) in bytes.iter().enumerate() { //enumerate() returns a tuple with index i
-        if item == b' ' { //byte literal syntx ' ' returns position i
+    for (i, &item) in bytes.iter().enumerate() {
+        //enumerate() returns a tuple with index i
+        if item == b' ' {
+            //byte literal syntx ' ' returns position i
             return &s[..i];
         }
     }
@@ -13,7 +14,6 @@ fn first_word(s: &str) -> &str {
 }
 
 fn main() {
-
     //String literals as slices
     let s: &str = "Hello, World"; //String literal
     let sl: &str = &s[..5]; //Reference to a slice
@@ -31,8 +31,11 @@ fn main() {
     //Slices
     let first_wordle: String = String::from("Peter is a weirdo");
     let w: &String = &first_wordle;
-    println!("The first word of the phrase \"{}\" is \"{}\"", first_wordle, first_word(w));
-
+    println!(
+        "The first word of the phrase \"{}\" is \"{}\"",
+        first_wordle,
+        first_word(w)
+    );
 
     let s: String = String::from("Hello, world!");
     let st: &str = &s;
@@ -40,5 +43,4 @@ fn main() {
     let hello: &str = &s[0..5];
     let world: &str = &s[7..12];
     println!("{hello} || {world}");
-
 }
