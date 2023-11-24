b874384a65efc108dfdae61374f32d0f1f73ed8c
p5chmitz <petermschmitz@gmail.com>
2023-11-23 23:19:34 -0800

    Clarifies the enums program

--- src/bin/enums.rs
+++ src/bin/enums.rs
@@ -10,33 +10,51 @@ fn route(a: IpAddrKind) {
 }
 
 #[derive(Debug)]
-struct TestingEnumsStruct {
-    one: String,
-    two: i32,
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
+impl Message {
+    fn set_move(x: i32, y: i32) -> Message {
+        Message::Move { x, y }
+    }
+    fn get_move(&self) -> &Message {
+        self
+    }
+}
+
+//Enum with different structs
 #[derive(Debug)]
-enum TestingEnumsEnum {
-    A(TestingEnumsStruct),
-    B(TestingEnumsStruct, String),
+enum Movement {
+    A(i32, i32),
+    B { x: i32, y: i32, z: i32 },
+    C(String)
 }
+
+
 fn main() {
-    //Builds enum instances
+    //Creates enum instances
     let addr1: IpAddrKind = IpAddrKind::V4(10, 0, 0, 1);
     let addr2: IpAddrKind = IpAddrKind::V6(String::from("2345:0425:2CA1:0000:0000:0567:5673:23b5"));
-
+    println!("{:?}", addr1);
     //Calls function that takes enum types
     route(addr2);
 
-    //Builds struct
-    let first: TestingEnumsStruct = TestingEnumsStruct {
-        one: String::from("Peter is definitely a person"),
-        two: 0,
-    };
-    let second: TestingEnumsStruct = TestingEnumsStruct {
-        one: String::from("Hello, world"),
-        two: 23,
-    };
-    let third = TestingEnumsEnum::A(first);
-    println!("{:#?}", third);
-    //println!("Greeting: {}", &first.one)
+    //Writes to field(s) and prints variant
+    let m: Message = Message::Write(String::from("A rising tide lifts all the homies"));
+    println!("m: {:#?}", m);
+    let cc: Message = Message::ChangeColor(12, 23, 1983);
+    println!("cc: {:#?}", cc);
+
+    //Writes to enum variant directly and prints variant
+    let mv: Message = Message::Move { x: 12, y: 23 };
+    println!("mva: {:#?}", mv);
+    
+    //Writes to enum variant and prints variant
+    let mva: Message = Message::set_move(12, 1983);
+    println!("mva: {:#?}", mva);
+    println!("IDK WTF: {:#?}", mva.get_move());
+
 }
