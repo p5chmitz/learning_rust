b8197a8fe9cb04c339a8ba46d8a32cfa7b13df46
p5chmitz <petermschmitz@gmail.com>
2023-11-24 22:59:10 -0800

    Makes major progress on enums/matching

--- src/bin/enums.rs
+++ src/bin/enums.rs
@@ -4,9 +4,18 @@ enum IpAddrKind {
     V4(u8, u8, u8, u8),
     V6(String),
 }
-//Function that takes specified enum type
-fn route(a: IpAddrKind) {
-    println!("The struct is: {:#?}", a)
+impl IpAddrKind {
+    fn access_and_print(a: IpAddrKind) {
+        match a {
+            IpAddrKind::V4(a, b, c, d) => {
+                println!("The IPv4 address is: {a}.{b}.{c}.{d}")
+            },
+            IpAddrKind::V6(a) => {
+                println!("The IPv6 address is: {a}");
+            },
+            _ => (),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -20,10 +29,27 @@ impl Message {
     fn set_move(x: i32, y: i32) -> Message {
         Message::Move { x, y }
     }
-    fn get_move(&self) -> &Message {
-        self
+    fn print_message(m: &Message) {
+        match m {
+            Message::Write(a) => {
+                println!("{a}")
+            },
+            _ => (),
+        }
+    }
+    fn get_message(m: &Message) -> String {
+        let mut s: String = String::new(); 
+        match m {
+            Message::Write(a) => {
+                s = a.to_string(); 
+            },
+            _ => (),
+        };
+        return s
     }
 }
+
+//The Coin enum
 enum Coin {
     Penny,
     Nickel,
@@ -33,19 +59,19 @@ enum Coin {
 fn value_of_coin(coin: Coin) -> u8 {
     match coin {
         Coin::Penny => {
-            println!("Thats a penny!");
+            println!("    Thats a penny!");
             1
         }
         Coin::Nickel => {
-            println!("Thats a nickel!");
+            println!("    Thats a nickel!");
             5
         }
         Coin::Dime => {
-            println!("Thats a dime!");
+            println!("    Thats a dime!");
             10
         }
         Coin::Quarter => {
-            println!("Thats a quarter!");
+            println!("    Thats a quarter!");
             25
         }
     }
@@ -55,7 +81,7 @@ fn value_of_coin(coin: Coin) -> u8 {
 #[derive(Debug)]
 enum Movement {
     A(i32, i32),
-    B { x: i32, y: i32, z: i32 },
+    //B { x: i32, y: i32, z: i32 },
     C(String),
 }
 fn testing_types(t: &Movement) -> String {
@@ -63,24 +89,41 @@ fn testing_types(t: &Movement) -> String {
     //Prints a message
     match t {
         Movement::A(..) => _decision = String::from("Yes! Its Movement type A"),
-        _other => _decision = String::from("(Type not found)"),
+        //Movement::C(..) => _decision = String::from("Yes! Its the Movement type C"),
+        _ => _decision = String::from("(Type not founderino)"),
     };
     return _decision;
+}
+fn my_docs_example(t: &Movement) {
+    match t {
+        Movement::A(..) => {
+            println!("Type A");
+        }
+        _ => {
+            println!("Type not found");
+        }
+    };
+}
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
 
 fn main() {
     //Creates enum instances
-    let addr1: IpAddrKind = IpAddrKind::V4(10, 0, 0, 1);
-    let addr2: IpAddrKind = IpAddrKind::V6(String::from("2345:0425:2CA1:0000:0000:0567:5673:23b5"));
-    println!("{:?}", addr1);
-    //Calls function that takes enum types
-    route(addr2);
+    //Calls associated function that takes enum types, prints values
+    let a1: IpAddrKind = IpAddrKind::V4(10, 0, 0, 1);
+    let a2: IpAddrKind = IpAddrKind::V6(String::from("2345:0425:2CA1:0000:0000:0567:5673:23b5"));
+    IpAddrKind::access_and_print(a1);
+    IpAddrKind::access_and_print(a2);
 
-    //Writes to field(s) and prints variant
+    //Creates enum instances
+    //Prints specific fields from Message using two different techniques 
     let m: Message = Message::Write(String::from("A rising tide lifts all the homies"));
-    println!("m: {:#?}", m);
-    let cc: Message = Message::ChangeColor(12, 23, 1983);
-    println!("cc: {:#?}", cc);
+    println!("The message is: {}", Message::get_message(&m));
+    Message::print_message(&m);
 
     //Writes to enum variant directly and prints variant
     let mv: Message = Message::Move { x: 12, y: 23 };
@@ -89,7 +132,7 @@ fn main() {
     //Writes to enum variant and prints variant
     let mva: Message = Message::set_move(12, 1983);
     println!("mva: {:#?}", mva);
-    println!("IDK WTF: {:#?}", mva.get_move());
+    //println!("IDK WTF: {:#?}", mva.get_move());
 
     //Introduces the Option<T> enum
     let some_number: Option<i32> = Some(5);
@@ -100,24 +143,38 @@ fn main() {
     dbg!(absent_value);
 
     //No such thing as null in Rust
-    let stringa: String = String::new();
-    println!("This is a null string: {stringa}");
-    let s: String = Default::default();
-    println!("This is a default string: {s}");
+    println!("1) Rust dont have no nulls");
+    let s1: String = String::new();
+    println!("    This is a null string: {s1}");
+    let s2: String = Default::default();
+    println!("    This is a default string: {s2}");
 
+    //Option<T> is a type that must be converted to its base value to be used
     let x: i32 = 12;
     let y: Option<i32> = Some(23);
     let z: i32 = y.unwrap();
     let xz: i32 = x + z;
-    println!("The value of the unwrapped value is: {xz}");
+    println!("2) The value of x is {x}, and the unwrapped Option<T> type is is: {z}");
+
+    //Adds one to the passed number using Option<T>
+    let five: Option<i32> = Some(5);
+    let none: Option<i32> = None;
+    println!("3) More use of the Option<T> type");
+    println!("    The value is now: {:?}", plus_one(five));
+    println!("    Testing: {:?}", five);
+    println!("    The value is now: {:?}", plus_one(Some(11)));
+    println!("    The value is now: {:?}", plus_one(none));
+    println!("    The value is now: {:?}", plus_one(None));
 
     //Introduces the match construct
     let input_coin: Coin = Coin::Quarter;
-    println!("The value of the coin is: {}", value_of_coin(input_coin));
+    println!("4) The value of the coin is:");
+    println!("{}", value_of_coin(input_coin));
 
+    //Passes a moement type and returns a string based on a match pattern
     let first: Movement = Movement::A(12, 23);
     let second: Movement = Movement::C(String::from("Hello!"));
-    println!("Is the input valid??");
+    println!("5) Is the input valid??");
     println!("    First test: {}", testing_types(&first));
     println!("    Second test: {}", testing_types(&second));
 }
