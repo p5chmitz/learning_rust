b5cad4847a5ef53b484a7122c891ebcf79c6180c
p5chmitz <petermschmitz@gmail.com>
2023-12-25 21:35:14 -0800

    Adds details about error propagation

--- src/cncpt/error_handling.rs
+++ src/cncpt/error_handling.rs
@@ -4,6 +4,7 @@
 use std::{
         fs::File, 
         io::{
+            self,
             Read, 
             ErrorKind
         }, 
@@ -19,7 +20,7 @@ pub fn error_handling_1() {
     //and extract either a file or an error 
     let greeting_result_handler = match greeting_result {
         Ok(file) => file,
-        Err(error) => panic!("Problem creating the file: {:?}", error),
+        Err(error) => panic!("There was a problem opening the file: {:?}", error),
     };
 }
 
@@ -40,7 +41,9 @@ pub fn error_handling_2() {
                 Ok(fc) => fc,
                 Err(e) => panic!("Problem creating the file: {:?}", e),
             },
-            //Catch-all as named identifier
+            //Catch-all as named identifier; The match arms must all
+            //return the same type; panic! can return the File type, but 
+            //println doesn't return anything, so this example shows a hack around that
             //other_error => panic!("Error: {:?}", other_error),
             other_error => {
                 println!("{:?}", other_error);
@@ -50,8 +53,64 @@ pub fn error_handling_2() {
     };
 }
 
+/**Introduces two methods for the Result<T, E> enum implementation
+ * with unwrap() and expect(); Both of these methods use the match 
+ * mechanism under the hood and panic when they encounter an Err; 
+ * Because of this, their use is generally discouraged by the Rust 
+ * language documentation; This function does the same thing as 
+ * error_handling_1() but combines the result with the result handler*/
+pub fn error_handling_3() {
+    //let greeting_file = File::open("hello.txt").unwrap();
+    let greeting_file = File::open("hello.txt")
+        .expect("hello.txt is required to proceed");
+}
+
+/**Uses error propagation to let the calling code handle errors; 
+ * Requires the use std::io::self statement;*/
+pub fn error_handling_4() -> Result<String, io::Error> {
+    let process_result = File::open("hello.txt");
+    let mut file_handler = match process_result {
+        Ok(file) => file,
+        //If an error is encountered the function returns the error 
+        //instead of panicking
+        Err(e) => return Err(e),
+    };
+    let mut contents = String::new();
+    match file_handler.read_to_string(&mut contents) {
+        Ok(_) => Ok(contents),
+        //Does the same thing as the previous match expression, 
+        //but we dont need an explicit return keyword Because 
+        //this is the last expression in the function
+        Err(e) => Err(e),
+    }
+}
+
+/**This function does the same as error_handling_4() but uses 
+ * the ? operator to dispatch with the boilerplate of the 
+ * match expressions*/
+pub fn error_handling_5() -> Result<String, io::Error> {
+   let mut process_result = File::open("./files/hello_wrld.txt")?;
+    let mut contents = String::new();
+    process_result.read_to_string(&mut contents)?;
+    Ok(contents) 
+}
+
+/**Error handling with closures; This code does the same thing as error_handling_2() but uses
+ * closures instead of match expressions*/
+pub fn error_handling_6() {
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
+
 /**rust-lang documentation example of File::open usage*/
-pub fn error_handling_3() -> std::io::Result<()> {
+pub fn error_handling_7() -> std::io::Result<()> {
     let mut file = File::open("hello.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
@@ -60,7 +119,7 @@ pub fn error_handling_3() -> std::io::Result<()> {
 }
 
 /***/
-pub fn error_handling_4() {
+pub fn error_handling_8() {
     //Sets location of test file
     let title = String::from("./files/hello.txt");
     //Creates an object of type Result<T, E> for the file's title
--- src/main.rs
+++ src/main.rs
@@ -101,7 +101,13 @@ fn main() {
     //cncpt::collections::vec_test_2(23);
     //cncpt::collections::vec_test_5(10);
     //cncpt::error_handling::error_handling_1();
-    cncpt::error_handling::error_handling_2();
+    //cncpt::error_handling::error_handling_2();
+    match cncpt::error_handling::error_handling_5() {
+        Ok(result) => println!("{}", result),
+        Err(e) => println!("Error: {}", e),
+
+    }; 
+    //println!("{:?}", s);
     //cncpt::io::io_1();
     //cncpt::io::io_2();
     //exmpl::json::json_parsing();
