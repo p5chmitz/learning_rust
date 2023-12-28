8796fdba17822139db48e979a1862c2215b78b08
p5chmitz <petermschmitz@gmail.com>
2023-12-27 17:28:34 -0800

    Adds tests module for main.rs access

--- src/exmpl/mod.rs
+++ src/exmpl/mod.rs
@@ -1,9 +1,11 @@
 pub mod guessing_game;
 pub mod json;
 pub mod rng_range;
+pub mod test;
 
 //Re-exports sub-module function for cleaner access in main.rs
 pub use guessing_game::guessing_game;
 pub use guessing_game::guessing_game_2;
+pub use guessing_game::guessing_game_3;
 pub use json::json_parsing;
 pub use rng_range::rng_range;
