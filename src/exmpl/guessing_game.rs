94e66427d76b4d55dc47761019dfd1fac440cc56
p5chmitz <petermschmitz@gmail.com>
2023-12-28 20:14:06 -0800

    Adds comments and another version of the game
    This refactors the code to implement range validation
    and panics if the guess is out of range. This isn't ideal
    and needs serious work, but exemplifies error handling
    and code modularity.

--- src/exmpl/guessing_game.rs
+++ src/exmpl/guessing_game.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 use rand::Rng;
 use std::cmp::*;
 use std::io::{self, Write};
@@ -16,7 +19,8 @@ pub fn guessing_game() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failure. You're a failure.");
-        //Shadows String guess and coverts to i32
+        //Shadows String guess and coverts to i32 with parse();
+        //parse() returns Result which is handled with a match mechanism
         let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -24,10 +28,6 @@ pub fn guessing_game() {
                 continue;
             }
         };
-        if guess <= 0 || guess >= 100 {
-            println!("Out of bounds, must be between 1-100");
-            continue;
-        }
         //println!("{}", &secret_number);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("{} is too smol", guess),
@@ -40,7 +40,7 @@ pub fn guessing_game() {
     }
 }
 
-/**My super hacky refactor attempt using if statements instead of pattern matching*/
+/**My super hacky refactor attempt; This code adds a range validator and if statements instead of pattern matching with the compare method*/
 pub fn guessing_game_2() {
     println!("Guess the number!");
     let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
@@ -51,7 +51,7 @@ pub fn guessing_game_2() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failure. You're a failure.");
-        //Shadows String guess and coverts to i32
+
         let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -63,7 +63,7 @@ pub fn guessing_game_2() {
             println!("Out of bounds, must be between 1-100");
             continue;
         }
-        //println!("{}", &secret_number);
+
         if guess < secret_number {
             println!("{} is too small", guess);
         } else if guess > secret_number {
@@ -72,5 +72,54 @@ pub fn guessing_game_2() {
             println!("Thats it! {} is the secret number!", secret_number);
             break;
         }
+    }
+}
+
+/**Validates guess with custom type which can reduce tedium of checking range for every instance
+ * this logic is required; Program panicks if the range is not an integer between 1-100*/
+pub fn guessing_game_3() {
+    println!("Guess the number!");
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+    loop {
+        print!("Enter guess: ");
+        io::stdout().flush().unwrap();
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failure. You're a failure.");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Guess must be an integer.");
+                continue;
+            }
+        };
+        Guess::new(guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{} is too smol", guess),
+            Ordering::Greater => println!("{} is too larg", guess),
+            Ordering::Equal => {
+                println!("You guessed it!");
+                break;
+            }
+        }
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value)
+        }
+        Guess { value }
+    }
+    pub fn value(&self) -> i32 {
+        self.value
     }
 }
