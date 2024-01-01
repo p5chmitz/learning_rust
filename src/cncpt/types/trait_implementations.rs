172d93fa9677e287973a7b26bef774462668290d
p5chmitz <petermschmitz@gmail.com>
2023-12-31 17:37:12 -0800

    Updates traits/implementation code

--- src/cncpt/types/trait_implementations.rs
+++ src/cncpt/types/trait_implementations.rs
@@ -1,35 +1,40 @@
-
-//===================================
-// News article
-
-/**Struct NewsArticle implements the Summary trait*/
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-//Implements the Summary trait for the NewsArticle type
-impl crate::cncpt::traits::Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
-}
-
-//===================================
-// Tweet
-
-/**Struct Tweet implements the Summary trait*/
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-//Implements the Summary trait for the Tweet type
-impl crate::cncpt::traits::Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
-}
-
+////===================================
+//// News article
+//
+//**Struct NewsArticle implements the Metadata trait*/
+//pub struct NewsArticle {
+//    pub headline: String,
+//    pub location: String,
+//    pub author: String,
+//    pub content: String,
+//}
+////Implements the Metadata trait for the NewsArticle type
+//impl crate::cncpt::traits::Metadata for NewsArticle {
+//    fn author(&self) -> String {
+//        self.author
+//    }
+//    fn summarize(&self) -> String {
+//        format!("{}, by {} ({})", self.headline, self.author, self.location)
+//    }
+//}
+//
+//
+////===================================
+//// Tweet
+//
+//**Struct Tweet implements the Metadata trait*/
+//pub struct Tweet {
+//    pub username: String,
+//    pub content: String,
+//    pub reply: bool,
+//    pub retweet: bool,
+//}
+////Implements the Metadata trait for the Tweet type
+//impl crate::cncpt::traits::Metadata for Tweet {
+//    fn author(&self) -> String {
+//        self.username
+//    }
+//    fn summarize(&self) -> String {
+//        format!("{}: {}", self.username, self.content)
+//    }
+//}
