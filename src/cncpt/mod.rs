83b899c051c8f3ad7579439415fae82a05780baa
p5chmitz <petermschmitz@gmail.com>
2024-01-21 16:32:47 -0800

    Adds test framework and function tests

--- src/cncpt/mod.rs
+++ src/cncpt/mod.rs
@@ -3,17 +3,17 @@ pub mod error_handling;
 pub mod io;
 pub mod lifetimes;
 pub mod ownership;
+pub mod test_framework;
 pub mod types;
 pub mod variables;
-pub mod test_framework;
 
 //Re-exports sub-modules for cleaner access in main.rs
 //Functions in these modules are accessed via cncpt::collecitons::function() or similar
 pub use ctrl_flow::ifs;
 pub use ctrl_flow::loops;
+pub use test_framework::function_tests;
 pub use types::compound::collections;
 pub use types::compound::strings;
 pub use types::compound::structs;
 pub use types::generics;
 pub use types::primitives::scalar;
-pub use test_framework::function_tests;
