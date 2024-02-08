d1a00c5083c741b5c3b45bc1c84ee77463ee8703
p5chmitz <petermschmitz@gmail.com>
2024-02-07 22:09:21 -0800

    Adds closures file

---
+++ src/cncpt/types/closures.rs
@@ -0,0 +1,14 @@
+
+
+pub fn closures_1(s: &str) -> Option<&str> {
+   if s.is_empty() {
+        None
+   } else {
+        Some(s)
+   } 
+} 
+
+pub fn printinator() -> String {
+    let s = String::from("Aint nothin here!");
+    s
+}
