80b407ab1d8891692aa11631e5a87b32d0b35b4e
p5chmitz <petermschmitz@gmail.com>
2024-08-18 00:16:28 -0700

    Adds a lot to the reference type examples

--- src/cncpt/types/primitives/reference.rs
+++ src/cncpt/types/primitives/reference.rs
@@ -1,23 +1,63 @@
+//=======
 // Slices
-/////////
-pub fn slices_1() {
-    let a = "Hello, Rust".to_string(); // Creates a string
-    let slice = &a[7..]; // Creates a reference to a substring slice of type &str
-    assert_eq!(slice, "Rust"); // Compares the slice to a static string "Rust"
+
+/** Illustrates slices over a basic integer array */
+pub fn slices_0() {
+    // Illustrates slicing a vector as a shared reference
+    // Base collection instance
+    let x = vec![1, 2, 3, 4, 5];
+
+    // Creates a slice of the whole vector
+    let whole_slice = &x[..];
+    assert_eq!(x, whole_slice);
+
+    // Creates a subset of the vector
+    let partial_slice = &x[0..3];
+    assert_eq!(partial_slice, vec![1, 2, 3]);
+
+    // Illustrates slicing a vector as a mutable reference
+    // Instantiates a mutable array
+    let mut y = [1, 2, 3];
+
+    // Shadows x to create a mutable slice of the whole original array
+    // It is only possible to create a mutable slice over a mutable base
+    let y = &mut y[..];
+
+    // Mutuates the value at index 1
+    y[1] = 7;
+
+    // Compares x to a slice literal
+    assert_eq!(y, &[1, 7, 3]);
+    // Compares x to an array literal
+    // Rust implicitly dereferences x
+    assert_eq!(y, [1, 7, 3]);
+    // Compares an explicitly dereferenced slice to an array literal
+    assert_eq!(*y, [1, 7, 3]);
 }
-// Slices are views into contiguous sequences, which means they can be iterated
+
+/** Illustrates slicing a string */
 pub fn slices_2() {
+    // Creates a String
+    let a = "Hello, Rust".to_string();
+    // Creates a reference to a substring slice of type &str
+    let slice = &a[7..];
+    assert_eq!(slice, "Rust");
+}
+
+/** Slices are views into contiguous sequences, which means they can be iterated */
+pub fn slices_3() {
     let s = "Hello, Rust".to_string(); // Creates a string
     let mut shout = String::new(); // Creates a mutable string to push new values
-    for c in s[7..].chars() { // chars() creates an iterator from a string slice (&str)
+    for c in s[7..].chars() {
+        // chars() creates an iterator from a string slice (&str)
         shout.push(c.to_ascii_uppercase())
     }
     assert_eq!(shout, "RUST".to_string()); // Proves the result
     println!("{}", s); // chars() takes a reference so s is still valid
 }
 
+//===========
 // References
-/////////////
 
 // Illustrates an immutable reference
 pub fn references_1() {
@@ -31,6 +71,7 @@ pub fn references_2() {
     // Simple mutable reference with inner scope
     let mut x = 23; // Creates a mutable variable
     {
+        // Creates a new scope for illustration, also works in native scope
         let y = &mut x; // Creates a mutable reference to the original variable
         *y += 60; // Mutates the mutable reference, effectively mutating the original variable
     }
@@ -46,11 +87,11 @@ pub fn references_2() {
 /** Illustrates immutable and mutable references */
 pub fn references_3() {
     // Heap-allocated value
-    let val = String::from("Peter"); 
+    let val = String::from("Peter");
     // Creates an immutable reference to the value
-    let val_ref = &val; 
+    let val_ref = &val;
     // Prints the referenced object
-    println!("Reference: {}", val_ref); 
+    println!("Reference: {}", val_ref);
     assert_eq!(*val_ref, "Peter".to_string()); // Need to deref &String to compare to String
     assert_eq!(val_ref, "Peter"); // Rust can automatically coerce &String to &str
 
@@ -67,14 +108,13 @@ pub fn references_3() {
 // Illustrates the dereference operator on a borrowed value
 pub fn references_4() {
     let v = vec!['a', 'b', 'c'];
-    // enumerate() is an iterator adapter that takes an iterator and returns an 
+    // enumerate() is an iterator adapter that takes an iterator and returns an
     // iterator that yields Option<(usize, <I as Iterator>::Item)>
-    // The loop body prints each index and its dereferenced value as 
+    // The loop body prints each index and its dereferenced value as
     for (i, val) in v.iter().enumerate() {
         println!("Element {} holds value {}", i, *val)
     }
 }
 
+//=========
 // Pointers
-///////////
-
